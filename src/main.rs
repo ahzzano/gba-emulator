98a445f204a92140e1555c21843eb1d7b197ca66
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-09-05 21:19:43 +0800

    feat: add first raylib window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use raylib::prelude::*;
+
+const GBA_RES_WIDTH: i32 = 240;
+const GBA_RES_HEIGHT: i32 = 160;
+
 fn main() {
-    println!("Hello, world!");
+    let (mut rl, thread) = raylib::init()
+        .size(GBA_RES_WIDTH, GBA_RES_HEIGHT)
+        .title("GBA Emulator")
+        .build();
+
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+
+        d.clear_background(Color::WHITE);
+        d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+    }
 }
