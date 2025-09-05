ce3718f213f1de45d90c523a29f52dd9009fb506
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-09-05 22:51:11 +0800

    feat: add better initializer to CPU

--- src/emulator/cpu.rs
+++ src/emulator/cpu.rs
@@ -1,5 +1,6 @@
 use crate::emulator::bus::Bus;
 
+const REG_SP: usize = 13;
 const REG_LR: usize = 14;
 const REG_PC: usize = 15;
 
@@ -17,12 +18,17 @@ pub struct CPU {
 
 impl Default for CPU {
     fn default() -> Self {
-        return Self {
+        let mut to_ret = Self {
             regs: [0; 16],
-            cpsr: 0,
+            cpsr: 0x6000_001F,
             spsr: [0; 6],
             bus: Box::new(Bus::default()),
         };
+
+        to_ret.regs[REG_SP] = 0x03007F00;
+        to_ret.regs[REG_PC] = 0x08000000;
+
+        to_ret
     }
 }
 
